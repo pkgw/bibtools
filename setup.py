4fb31e4f01cba820df265cbc7cb7786864bf4e65
Peter Williams <peter@newton.cx>
2017-08-02 13:26:53 -0400

    Bump to development version 0.3.0.99

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup (
     name = 'bibtools',
-    version = '0.3',
+    version = '0.3.0.99',
 
     # This package actually *is* zip-safe, but I've run into issues with
     # installing it as a Zip: in particular, the install sometimes fails with

1ed16afcc66f0ee0e6f7af1a1ad55798a9b734a0
Peter Williams <peter@newton.cx>
2018-09-18 16:41:28 -0400

    bibtools/webutil.py: function call whitespace futzing

--- bibtools/webutil.py
+++ bibtools/webutil.py
@@ -37,7 +37,7 @@ urlparse
 urlquote
 urlunparse
 urlunquote
-''').split ()
+''').split()
 
 
 build_opener = request.build_opener
@@ -59,72 +59,72 @@ except ImportError:
     from HTMLParser import HTMLParser
 
 
-class NonRedirectingProcessor (request.HTTPErrorProcessor):
+class NonRedirectingProcessor(request.HTTPErrorProcessor):
     # Copied from StackOverflow q 554446.
-    def http_response (self, request, response):
+    def http_response(self, request, response):
         return response
 
     https_response = http_response
 
 
-class DebugRedirectHandler (request.HTTPRedirectHandler):
+class DebugRedirectHandler(request.HTTPRedirectHandler):
     """Shouldn't be used in production code, but useful for proxy debugging."""
 
     def redirect_request(self, req, fp, code, msg, headers, newurl):
         import sys
-        print ('REDIRECT:', req.get_method (), code, newurl, file=sys.stderr)
-        return request.HTTPRedirectHandler.redirect_request (self, req, fp, code, msg, headers, newurl)
+        print('REDIRECT:', req.get_method(), code, newurl, file=sys.stderr)
+        return request.HTTPRedirectHandler.redirect_request(self, req, fp, code, msg, headers, newurl)
 
 
-def get_url_from_redirection (url):
+def get_url_from_redirection(url):
     """Note that we don't go through the proxy class here for convenience, under
     the assumption that all of these redirections involve public information
     that won't require privileged access."""
 
-    opener = request.build_opener (NonRedirectingProcessor ())
-    resp = opener.open (url)
+    opener = request.build_opener(NonRedirectingProcessor())
+    resp = opener.open(url)
 
     if resp.code not in (301, 302, 303, 307) or 'Location' not in resp.headers:
-        die ('expected a redirection response for URL %s but didn\'t get one', url)
+        die('expected a redirection response for URL %s but didn\'t get one', url)
 
-    resp.close ()
+    resp.close()
     return resp.headers['Location']
 
 
-def parse_http_html (resp, parser):
+def parse_http_html(resp, parser):
     """`parser` need only have two methods: `feed()` and `close()`."""
 
     debug = False # XXX hack
 
     if six.PY2:
-        charset = resp.headers.getparam ('charset')
+        charset = resp.headers.getparam('charset')
     else:
         charset = resp.headers.get_content_charset('ISO-8859-1')
 
     if charset is None:
         charset = 'ISO-8859-1'
 
-    dec = codecs.getincrementaldecoder (charset) ()
+    dec = codecs.getincrementaldecoder(charset)()
 
     if debug:
-        f = open ('debug.html', 'w')
+        f = open('debug.html', 'w')
 
     while True:
-        d = resp.read (4096)
-        if not len (d):
-            text = dec.decode (b'', final=True)
-            parser.feed (text)
+        d = resp.read(4096)
+        if not len(d):
+            text = dec.decode(b'', final=True)
+            parser.feed(text)
             break
 
         if debug:
-            f.write (d)
+            f.write(d)
 
-        text = dec.decode (d)
-        parser.feed (text)
+        text = dec.decode(d)
+        parser.feed(text)
 
     if debug:
-        f.close ()
+        f.close()
 
-    resp.close ()
-    parser.close ()
+    resp.close()
+    parser.close()
     return parser

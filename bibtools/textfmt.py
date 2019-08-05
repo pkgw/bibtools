cb6490552a110af52252ac71cc54f9ce15089194
Peter Williams <peter@newton.cx>
2019-08-05 13:01:30 -0400

    bibtools/textfmt.py: function call whitespace futzing

--- bibtools/textfmt.py
+++ bibtools/textfmt.py
@@ -14,133 +14,133 @@ import json
 from .util import *
 from .bibcore import *
 
-__all__ = ('export_one import_one').split ()
+__all__ = ('export_one import_one').split()
 
 
-def export_one (app, pub, stream, width):
+def export_one(app, pub, stream, width):
     write = stream.write
 
     # Title and year
     if pub.title is None:
-        write ('--no title--\n')
+        write('--no title--\n')
     else:
-        print_linewrapped (pub.title, width=width, stream=stream)
+        print_linewrapped(pub.title, width=width, stream=stream)
     if pub.year is None:
-        write ('--no year--\n')
+        write('--no year--\n')
     else:
-        write (text_type (pub.year))
-        write ('\n')
-    write ('\n')
+        write(text_type(pub.year))
+        write('\n')
+    write('\n')
 
     # Unique identifiers
-    write ('arxiv = ')
-    write (pub.arxiv or '')
-    write ('\n')
-    write ('bibcode = ')
-    write (pub.bibcode or '')
-    write ('\n')
-    write ('doi = ')
-    write (pub.doi or '')
-    write ('\n')
-    for (nick, ) in app.db.execute ('SELECT nickname FROM nicknames WHERE pubid == ? '
-                                    'ORDER BY nickname asc', (pub.id, )):
-        write ('nick = ')
-        write (nick)
-        write ('\n')
-    write ('\n')
+    write('arxiv = ')
+    write(pub.arxiv or '')
+    write('\n')
+    write('bibcode = ')
+    write(pub.bibcode or '')
+    write('\n')
+    write('doi = ')
+    write(pub.doi or '')
+    write('\n')
+    for (nick, ) in app.db.execute('SELECT nickname FROM nicknames WHERE pubid == ? '
+                                   'ORDER BY nickname asc', (pub.id, )):
+        write('nick = ')
+        write(nick)
+        write('\n')
+    write('\n')
 
     # Authors
     anyauth = False
-    for given, family in app.db.get_pub_authors (pub.id, 'author'):
-        write (encode_name (given, family))
-        write ('\n')
+    for given, family in app.db.get_pub_authors(pub.id, 'author'):
+        write(encode_name(given, family))
+        write('\n')
         anyauth = True
     if not anyauth:
-        write ('--no authors--\n')
+        write('--no authors--\n')
     firsteditor = True
-    for given, family in app.db.get_pub_authors (pub.id, 'editor'):
+    for given, family in app.db.get_pub_authors(pub.id, 'editor'):
         if firsteditor:
-            write ('--editors--\n')
+            write('--editors--\n')
             firsteditor = False
-        write (encode_name (given, family))
-        write ('\n')
-    write ('\n')
+        write(encode_name(given, family))
+        write('\n')
+    write('\n')
 
     # Reference info
     if pub.refdata is None:
-        write ('--no reference data--\n')
+        write('--no reference data--\n')
     else:
-        rd = json.loads (pub.refdata)
+        rd = json.loads(pub.refdata)
 
-        btype = rd.pop ('_type')
-        write ('@')
-        write (btype)
-        write ('\n')
+        btype = rd.pop('_type')
+        write('@')
+        write(btype)
+        write('\n')
 
-        for k in sorted (rd.keys ()):
-            write (k)
-            write (' = ')
-            write (rd[k])
-            write ('\n')
-    write ('\n')
+        for k in sorted(rd.keys()):
+            write(k)
+            write(' = ')
+            write(rd[k])
+            write('\n')
+    write('\n')
 
     # Abstract
     if pub.abstract is None:
-        write ('--no abstract--\n')
+        write('--no abstract--\n')
     else:
-        print_linewrapped (pub.abstract, width=width, stream=stream, maxwidth=72)
-    write ('\n')
+        print_linewrapped(pub.abstract, width=width, stream=stream, maxwidth=72)
+    write('\n')
 
     # TODO: notes, lists
 
 
-def _import_get_chunk (stream, gotoend=False):
+def _import_get_chunk(stream, gotoend=False):
     lines = []
 
     for line in stream:
-        line = line.strip ()
+        line = line.strip()
 
-        if not len (line) and not gotoend:
+        if not len(line) and not gotoend:
             return lines
 
-        lines.append (line)
+        lines.append(line)
 
-    while len (lines) and not len (lines[-1]):
+    while len(lines) and not len(lines[-1]):
         lines = lines[:-1]
 
     return lines
 
 
-def import_one (stream):
+def import_one(stream):
     info = {}
 
     # title / year
-    c = _import_get_chunk (stream)
+    c = _import_get_chunk(stream)
 
-    if len (c) < 2:
-        die ('title/year chunk must contain at least two lines')
+    if len(c) < 2:
+        die('title/year chunk must contain at least two lines')
 
-    info['title'] = squish_spaces (' '.join (c[:-1]))
-    if info['title'].startswith ('--'):
+    info['title'] = squish_spaces(' '.join(c[:-1]))
+    if info['title'].startswith('--'):
         del info['title']
 
-    if not c[-1].startswith ('--'):
+    if not c[-1].startswith('--'):
         try:
-            info['year'] = int (c[-1])
+            info['year'] = int(c[-1])
         except Exception as e:
-            die ('publication year must be an integer or "--no year--"; '
-                 'got "%s"', c[-1])
+            die('publication year must be an integer or "--no year--"; '
+                'got "%s"', c[-1])
 
     # identifiers
-    c = _import_get_chunk (stream)
+    c = _import_get_chunk(stream)
     info['nicknames'] = []
 
     for line in c:
         if '=' not in line:
-            die ('identifier lines must contain "=" signs; got "%s"', line)
-        k, v = line.split ('=', 1)
-        k = k.strip ()
-        v = v.strip ()
+            die('identifier lines must contain "=" signs; got "%s"', line)
+        k, v = line.split('=', 1)
+        k = k.strip()
+        v = v.strip()
 
         if not v:
             continue
@@ -152,12 +152,12 @@ def import_one (stream):
         elif k == 'doi':
             info['doi'] = v
         elif k == 'nick':
-            info['nicknames'].append (v)
+            info['nicknames'].append(v)
         else:
-            die ('unexpected identifier kind "%s"', k)
+            die('unexpected identifier kind "%s"', k)
 
     # authors
-    c = _import_get_chunk (stream)
+    c = _import_get_chunk(stream)
     namelist = info['authors'] = []
 
     for line in c:
@@ -167,33 +167,33 @@ def import_one (stream):
         elif line == '--editors--':
             namelist = info['editors'] = []
         else:
-            namelist.append (line)
+            namelist.append(line)
 
     # reference data
-    c = _import_get_chunk (stream)
+    c = _import_get_chunk(stream)
 
-    if not c[0].startswith ('--'):
+    if not c[0].startswith('--'):
         rd = info['refdata'] = {}
 
         if c[0][0] != '@':
-            die ('reference data chunk must begin with an "@"; got "%s"', c[0])
+            die('reference data chunk must begin with an "@"; got "%s"', c[0])
         rd['_type'] = c[0][1:]
 
         for line in c[1:]:
             if '=' not in line:
-                die ('ref data lines must contain "=" signs; got "%s"', line)
-            k, v = line.split ('=', 1)
-            k = k.strip ()
-            v = v.strip ()
+                die('ref data lines must contain "=" signs; got "%s"', line)
+            k, v = line.split('=', 1)
+            k = k.strip()
+            v = v.strip()
             rd[k] = v
 
     # abstract
-    c = _import_get_chunk (stream, gotoend=True)
+    c = _import_get_chunk(stream, gotoend=True)
     abs = ''
     spacer = ''
 
     for line in c:
-        if not len (line):
+        if not len(line):
             spacer = '\n'
         elif line == '--no abstract--':
             pass # exact match here too; legitimate lines could start with '--'
@@ -201,7 +201,7 @@ def import_one (stream):
             abs += spacer + line
             spacer = ' '
 
-    if len (abs):
+    if len(abs):
         info['abstract'] = abs
 
     return info
